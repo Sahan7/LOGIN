f53b0bd0990cecd6dc81f4c1831eb1b70dfa44f8
Sahan7 <sahan.pasindupeiris7@gmail.com>
2022-12-30 00:55:48 +0530

    loginPart

---
+++ app/src/main/java/com/example/mystore/User.java
@@ -0,0 +1,16 @@
+package com.example.mystore;
+
+public class User {
+    public String firstName;
+    public String email;
+
+    public User(){
+
+    }
+
+    public User(String firstName, String email){
+        this.firstName = firstName;
+        this.email = email;
+
+    }
+}
